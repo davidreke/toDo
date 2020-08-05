6f5cfd886a119b42b1150eb52cdeb274d8319509
David Reke <david.e.reke@gmail.com>
2020-08-05 09:22:48 -0400

    Updates

---
+++ precache-manifest.bc159ba42d4d45fac27f4d400eb87df6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b40a0d3984f295462d2c07673784eee",
+    "url": "/http/davidreke.github.io/toDo/index.html"
+  },
+  {
+    "revision": "ddbd2643345fee9f1a1d",
+    "url": "/http/davidreke.github.io/toDo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ed1012d95769e620f308",
+    "url": "/http/davidreke.github.io/toDo/static/js/2.b8f6d5ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http/davidreke.github.io/toDo/static/js/2.b8f6d5ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddbd2643345fee9f1a1d",
+    "url": "/http/davidreke.github.io/toDo/static/js/main.3c71b0ea.chunk.js"
+  },
+  {
+    "revision": "62994ae08506dd7ee340",
+    "url": "/http/davidreke.github.io/toDo/static/js/runtime-main.b61f7e6e.js"
+  }
+]);
